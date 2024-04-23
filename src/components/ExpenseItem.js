55bd9f56a3e73a5effea8f04933fdaede6b8494e
amitkocher1314 <amitkocher7206251314@gmail.com>
2024-04-23 20:10:22 +0530

    task-6-js-inside-jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,9 @@
 import "./ExpenseItem.css" ;
 function ExpenseItem(){
+    const expensedate = new Date(2024, 4, 23).toISOString();
+    const expensetitle = "Insurance";
+    const expenseloc = "Bangalore";
+    const expenseamount = "50"
     return(<>
     <p>This is Expense Item</p>
     <p>Food - $5</p>
@@ -18,7 +22,18 @@ function ExpenseItem(){
         <h2>Book</h2>
         <div className="expense-item__price">$10</div>
       </div>
-   </>)
+
+   <h1>Task 6</h1>
+   <div className="expense-item">
+      <div>{expensedate}</div>
+      <div className="expense-item__location">{expenseloc}</div>
+      <div className="expense-item__description">
+        <h2>{expensetitle}</h2>
+        <div className="expense-item__price">${expenseamount}</div>
+      </div>
+    </div>
+   </>
+   )
    
 }
 export default ExpenseItem;
