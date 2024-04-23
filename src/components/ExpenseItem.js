cbf8c947361a18216f752d04766cc2d4478bc000
amitkocher1314 <amitkocher7206251314@gmail.com>
2024-04-23 17:30:14 +0530

    making custom component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return(<>
+    <p>This is Expense Item</p>
+    <p>Food - $5</p>
+
+    <p>Petrol - $20</p>
+    
+   <p>Movie - $10</p> 
+   </>)
+   
+}
+export default ExpenseItem;
