f25b5c42c546832032659d54bdf59aefc89155da
amitkocher1314 <amitkocher7206251314@gmail.com>
2024-04-23 18:34:14 +0530

    Task-5-Styling-components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import "./ExpenseItem.css" ;
 function ExpenseItem(){
     return(<>
     <p>This is Expense Item</p>
@@ -6,6 +7,17 @@ function ExpenseItem(){
     <p>Petrol - $20</p>
     
    <p>Movie - $10</p> 
+
+   <h1>TASK 4 </h1>
+      <div className="expense-item"> 15 August 2023
+      <div className="expense-item__location">
+        Delhi
+      </div>
+       </div>
+      <div className="expense-item__description">
+        <h2>Book</h2>
+        <div className="expense-item__price">$10</div>
+      </div>
    </>)
    
 }
