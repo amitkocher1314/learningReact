58c1f8b0674c10595190dcb30fb1dcf8886b8b19
amitkocher1314 <amitkocher7206251314@gmail.com>
2024-04-23 18:25:08 +0530

    task-4-complex-jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
-  return (
+  return (<>
     <div>
        <h1>Learn React Started</h1>   
        <ExpenseItem></ExpenseItem>
      </div>
+
+      
+      <h1>TASK 4 </h1>
+      <div> 15 August 2023
+      <div>
+        Delhi
+      </div>
+       </div>
+      <div>
+        <h2>Book</h2>
+        <div>$10</div>
+      </div>
+      </>
   );
 }
 
